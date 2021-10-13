4457034a8ab2da4541439db8735cea4188a705c4
Jamie <jamiehuff737@gmail.com>
2021-10-13 08:35:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import './App.css';
+import { increment, decrement } from "./actions"
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  /* Goals:
+    1. Create a page with two tables side by side in the center of the page
+    2. Fill each row with 10 unique breeds from the api, rank them in order
+      - All breeds must be unique, none of the 20 breeds can be the same
+      - Breeds should be new on each refresh
+    3. Table state must be implemented using react
+  */
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'top-head'}>
+        <img src="/images/spg-head.png" className={'top-bar-logo'}></img>
+        <div>
+      <h1 className={'top-head-text'}>Dog Ranker 5000</h1>
+      </div>
+      </div>
+
+      <h1> Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(1))}> Add 1 + </button>
+      <button onClick={() => dispatch(increment(5))}> Add 5 + </button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+const [dogBreeds, setDogBreeds] = useState(null);
+
+  const getDogBreeds = async () => {
+    try{
+      const { data } = await axios.get('https://dog.ceo/api/breeds/list/all')
+      if(data) {
+        const newDogList = data
+        setDogBreeds(newDogList);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  useEffect(() => {
+    getDogBreeds()
+  }, []);
+
+  return {
+    dogBreeds,
+    setDogBreeds
+  }
+}
